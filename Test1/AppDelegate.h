8ad441f78ed0d886072ea9ced71a822e7e2b6d16
qingjie <qingjie@qingjies-macbook-pro.local>
2015-09-30 11:14:57 -0400

    Initial Commit

---
+++ Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by qingjie on 9/30/15.
+//  Copyright © 2015 qingjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by qingjie on 9/30/15.
+//  Copyright © 2015 qingjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
